2838bf2333f5e1a8c066dafcdc8fa8a501490e33
Jeremy Ramos <ramos.jeremykent@gmail.com>
2023-09-28 21:38:48 +0800

    chore: moved main article content to definition file

---
+++ src/app/definitions/main-article.ts
@@ -0,0 +1,32 @@
+export const mainArticleContent = {
+  id: 1,
+  title: "ANSWER YOUR BODY'S NEEDS",
+  content:
+    "The way ingredients are sourced affects the way we nourish our\
+      bodies. Author Mark Schatzer believes our body naturally develops\
+      an appetite for the foods and nutrients it needs to be healthy,\
+      but that artificial flavourings are getting in the way. This can\
+      be reversed by focusing on high-quality ingredients and being\
+      mindful as your appetite guides you to consume according to your\
+      body's needs",
+  footer: {
+    title: "BE MINDFUL",
+    content:
+      "  Sourcing local or organic food is a good way to start being more\
+              mindful about what you're cooking and eating",
+  },
+  images: [
+    {
+      src: "/assets/component-01/Image-01@2x.jpg",
+      alt: "Image 01",
+    },
+    {
+      src: "/assets/component-01/Image-02@2x.jpg",
+      alt: "Image 02",
+    },
+    {
+      src: "/assets/component-01/Image-03@2x.jpg",
+      alt: "Image 03",
+    },
+  ],
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,44 @@
 "use client";
 
-import Image from "next/image";
+import { mainArticleContent } from "./definitions/main-article";
 
 export default function Home() {
   return (
     <main className="min-h-screen py-24 px-28">
-      <div className="grid md:grid-cols-3">
-        <div className="col-span-2"></div>
+      <div className="grid md:grid-cols-3 gap-x-5">
+        <div className="col-span-2">
+          <div className="flex h-full items-stretch gap-5">
+            <div>
+              <img src="/assets/component-01/Image-01@2x.jpg" alt="Image 01" />
+            </div>
+            <div className="space-y-5">
+              <img src="/assets/component-01/Image-02@2x.jpg" alt="Image 02" />
+              <img src="/assets/component-01/Image-03@2x.jpg" alt="Image 03" />
+            </div>
+          </div>
+        </div>
         <div>
           <div className="space-y-2 mb-9">
             <h1 className="md:pr-5 text-[1.8rem] uppercase font-openSansLight pb-1">
-              ANSWER YOUR BODY'S NEEDS
+              {mainArticleContent.title}
             </h1>
             <hr />
-            <p className="leading-9 pt-4 text-lg font-openSansLight">
-              The way ingredients are sourced affects the way we nourish our
-              bodies. Author Mark Schatzer believes our body naturally develops
-              an appetite for the foods and nutrients it needs to be healthy,
-              but that artificial flavourings are getting in the way. This can
-              be reversed by focusing on high-quality ingredients and being
-              mindful as your appetite guides you to consume according to your
-              body's needs
+            <p className="leading-9 pt-4 text-lg font-openSansLight text-gray-300">
+              {mainArticleContent.content}
             </p>
           </div>
           <div className="space-y-3">
-            <p className="text-lg text-red-700 font-openSansRegular">
-              BE MINDFUL
+            <p className="text-lg text-red-700 font-openSansBold">
+              {mainArticleContent.footer.title}
             </p>
             <p className="text-lg font-openSansBold">
-              Sourcing local or organic food is a good way to start being more
-              mindful about what you're cooking and eating
+              {mainArticleContent.footer.content}
             </p>
           </div>
         </div>
       </div>
       <div className="pt-20">
-        <p className="uppercase text-5xl text-center">
+        <p className="uppercase text-5xl text-center font-openSansLight">
           All the lastest from AEG
         </p>
       </div>
