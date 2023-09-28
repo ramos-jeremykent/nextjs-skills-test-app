fc80d2acb0377730d35f8ffbdc8007639ecf4595
Jeremy Ramos <ramos.jeremykent@gmail.com>
2023-09-28 21:12:46 +0800

    chore: added article cards content

---
+++ src/app/definitions/articles.ts
@@ -0,0 +1,26 @@
+export const articlesData = [
+  {
+    id: 1,
+    title: "Summer Lunch Menu By Mark Best",
+    content:
+      "AEG ambassador Mark Best's summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home.",
+    link: "#/",
+    main_photo: "/assets/component-02/Image-01@2x.jpg",
+  },
+  {
+    id: 2,
+    title: "A Traditional Christmas Eve, Mark Best Style",
+    content:
+      "One of Australia's best chefs and AEG ambassador, Mark Best, shares his favourite Christmas Eve menu which is sure to impress your guests.",
+    link: "#/",
+    main_photo: "/assets/component-02/Image-02@2x.jpg",
+  },
+  {
+    id: 2,
+    title: "Taking Taste Further",
+    content:
+      "This exclusive cookbook gives you all the know-how you need. We've designed it to make sure you get the most out of our products - and the best out of your food.",
+    link: "#/",
+    main_photo: "/assets/component-02/Image-03@2x.jpg",
+  },
+];
